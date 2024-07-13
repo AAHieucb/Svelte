74219d16b9da04d0e7b9833f0271c077d6010604
Ryan.Nguyen <Ryan.Nguyen@avepoint.com>
2024-07-13 11:16:28 +0700

    a

---
+++ Code/basic/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nested from './nestedtest.svelte';
+	let src = '/static/favicon.png';
+	let name = 'Svelte';
+	let string = `this string contains some <strong>HTML!!!</strong>`;
+</script>
+
+<img {src} alt="{name} dances." />
+<h1>Hello {name.toUpperCase()}!</h1>
+<p>{@html string}</p>
+<Nested answer={42} /> 
+<Nested {...{answer: 42}} />
---
+++ Code/basic/src/routes/nestedtest.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let answer = "Unset default value";
+	let test = answer;
+	let count = 0;
+	$: doubled = count * 2;
+  $: console.log(`the count is ${count}`);
+  $: {
+    console.log(`the count is ${count}`);
+    console.log(`this will also be logged whenever count changes`);
+  }
+  $: if (count >= 10) {
+		alert('count is dangerously high!');
+		count = 0;
+	}
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+<p>{count} doubled is {doubled}</p>
+<p>The answer is {answer}</p>
+<p>The answer is {test}</p>
